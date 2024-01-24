a13992f1566756cba1be399240bc6bb3d7b4f42c
Jacob Lister <jacob.h.lister@gmail.com>
2024-01-24 23:58:31 +1300

    multi button handling

--- arduino/spingate/spingate.ino
+++ arduino/spingate/spingate.ino
@@ -93,7 +93,7 @@ int readButton(int index)
 
 void writeSignals(int op, int clock_gate, int clock_spin)
 {
-  PORTB = (PORTB & 0b11111000) | 
+  PORTB = (PORTB & 0b11111000) |
           (op ? (1 << 2) : 0) |
           (clock_gate ? (1 << 1) : 0) |
           (clock_spin ? (1 << 0) : 0);
@@ -105,36 +105,69 @@ void setup()
   setupHardWare();
 }
 
-int readButtons(void) {
-  int result = 0;
-  static int button0 = 0;
-  static int button1 = 0;
-  static int button2 = 0;
+#define BUTTON_DEBOUNCE_PERIOD 30
+#define BUTTON_RELEASE_PERIOD 150
+#define BUTTON_MAX_PERIOD 1000
 
-  int button0_next = readButton(0);
-  int button1_next = readButton(1);
-  int button2_next = readButton(2);
+struct ButtonState
+{
+  byte state;
+  byte count;
+  short period;
+};
 
-  if (button0_next && button0_next != button0)
+struct ButtonState buttonState[3];
+
+void signalButton(struct ButtonState *button, byte state)
+{
+  if (button->state == state || button->period < BUTTON_DEBOUNCE_PERIOD)
   {
-    result = 1;
+    if (button->period < BUTTON_MAX_PERIOD)
+    {
+      button->period++;
+    }
+  }
+  else
+  {
+    if (state)
+    {
+      button->count++;
+    }
+    button->state = state;
+    button->period = 0;
+  }
+}
+
+void updateButtons(void)
+{
+  for (int i = 0; i < 3; i++)
+  {
+    signalButton(&buttonState[i], readButton(i));
+  }
+}
+
+int clickButton(int index)
+{
+  int count = 0;
+  if (buttonState[index].count > 0 && buttonState[index].state == 1)
+  {
+    count = buttonState[index].count;
+    buttonState[index].count = 0;
   }
 
-  if (button1_next && button1_next != button1)
+  return count;
+}
+
+int countButton(int index)
+{
+  int count = 0;
+  if (buttonState[index].count > 0 && buttonState[index].state == 0 && buttonState[index].period > BUTTON_RELEASE_PERIOD)
   {
-    result = 2;
+    count = buttonState[index].count;
+    buttonState[index].count = 0;
   }
 
-  if (button2_next && button2_next != button2)
-  {
-    result = 3;
-  }
-
-  button0 = button0_next;
-  button1 = button1_next;
-  button2 = button2_next;
-
-  return result;
+  return count;
 }
 
 byte progSpin[] = {0x00, 0x01, 0x00};
@@ -142,53 +175,78 @@ byte progGate[] = {0x00, 0x01, 0x80};
 // byte progSpinBack[] = {0x00, 0x0C, 0x00};
 // byte progSpinBack[] = {0x00, 0x04, 0xC0};
 byte progSpinBack[] = {0x00, 0x0F, 0x18, 0x00};
-byte *progActive = NULL; 
+byte *progActive = NULL;
 int progIndex = 0;
 int progPhase = 0;
-int divider = 0;
 
-#define DIVIDER 1
+#define PROG_DIVIDER 1
+#define BUTTON_DIVIDER 1000
+int progDivider = 0;
+int buttonDivider = 0;
 
 void loop()
 {
-  if (progActive) {
-    if (divider == 0) {
+  if (buttonDivider == 0)
+  {
+    updateButtons();
+    buttonDivider = BUTTON_DIVIDER;
+
+    if (!progActive)
+    {
+      int button = countButton(0);
+      switch (button)
+      {
+      case 1:
+        progActive = progSpin;
+        break;
+      case 2:
+        progActive = progGate;
+        break;
+      }
+
+      if (clickButton(1)) {
+        progActive = progSpinBack;
+      }
+
+      if (progActive)
+      {
+        progIndex = 0;
+        progPhase = 0;
+      }
+    }
+  }
+  buttonDivider--;
+
+  if (progActive)
+  {
+    if (progDivider == 0)
+    {
       int length = progActive[1];
       int bit = (progActive[(progIndex >> 3) + 2] >> (7 - (progIndex & 7))) & 1;
-      if (progPhase == 0) {
-        if (progIndex == length) {
+      if (progPhase == 0)
+      {
+        if (progIndex == length)
+        {
           progActive = NULL;
           bit = 0;
         }
         writeSignals(bit, 0, 0);
 
         progPhase = 1;
-      } else {
+      }
+      else
+      {
         writeSignals(bit, 1, !bit);
 
         progPhase = 0;
         progIndex++;
       }
 
-      divider = DIVIDER;
-    } else {
-      divider--;
+      progDivider = PROG_DIVIDER;
     }
-  } else {
-    int button = readButtons();
-    if (button != 0) {
-      if (button == 1) {
-        progActive = progSpin;
-      }
-      if (button == 2) {
-        progActive = progGate;
-      }
-      if (button == 3) {
-        progActive = progSpinBack;
-      }
-
-      progIndex = 0;
-      progPhase = 0;
+    else
+    {
+      progDivider--;
     }
   }
 
