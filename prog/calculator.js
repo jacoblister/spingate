7d45520f421c40c7a9adee94d4f44614789cce7d
Jacob Lister <jacob.h.lister@gmail.com>
2024-01-24 19:54:46 +1300

    improve calculator build size

--- prog/calculator.js
+++ prog/calculator.js
@@ -40,34 +40,38 @@ add4 = (src, dst, carry, tmpA, tmpB) =>
     fulladd(src[3], dst[3], carry, tmpA, tmpB)
 
 all = [A, B, C, D, E, F, G]
-dig = [
+digit = [
     [A, B, C, D, E, F], [B, C], [A, B, D, E, G], [A, B, C, D, G],
     [B, C, F, G], [A, C, D, F, G], [A, C, D, E, F, G], [A, B, C],
     [A, B, C, D, E, F, G], [A, B, C, D, F, G], [A, B, C, E, F, G], [C, D, E, F, G],
     [A, D, E, F], [B, C, D, E, G], [A, D, E, F, G], [A, E, F, G]
 ]
-num = [
+invertedDigit = digit.map((dig) => all.reduce((acc, bit) => dig.includes(bit) ? acc : acc.concat(bit), []))
+
+value = [
     [], [A0], [A1], [A0, A1], [A2], [A2, A0], [A2, A1], [A2, A1, A0],
     [A3], [A3, A0], [A3, A1], [A3, A0, A1], [A3, A2], [A3, A2, A0], [A3, A2, A1], [A3, A2, A1, A0]
 ]
+invertedValue = value.map((dig) => all.reduce((acc, bit) => dig.includes(bit) ? acc : acc.concat(bit), []))
+
 
 clearBits = (bits) => bits.reduce((acc, bit) => acc + clear(bit), "")
-segToggle = (dig) => dig.reduce((acc, bit) => acc + toggle(bit), "")
-segClearIf = (dig, DP) => dig.reduce((acc, bit) => acc + and(DP, bit), "")
-segSetIf = (dig, DP) => dig.reduce((acc, bit) => acc + or(DP, bit), "")
-test = (pattern, bits, dst) => clear(dst) +
+segToggle = (digit) => digit.reduce((acc, bit) => acc + toggle(bit), "")
+segClearIf = (digit, DP) => digit.reduce((acc, bit) => acc + and(DP, bit), "")
+segSetIf = (digit, DP) => digit.reduce((acc, bit) => acc + or(DP, bit), "")
+test = (pattern, bits, dst) => set(dst) +
     pattern.reduce((acc, bit) => acc + toggle(bit), "") + 
-    bits.reduce((acc, bit) => acc + or(bit, dst), "") +
-    pattern.reduce((acc, bit) => acc + toggle(bit), "")
+    bits.reduce((acc, bit) => acc + and(bit, dst), "") +
+    pattern.reduce((acc, bit) => acc + toggle(bit), "") + toggle(DP)
 convert = (from, to) => test(from, all, DP) + segClearIf(all, DP) + toggle(DP) + segSetIf(to, DP) + clear(DP)
-toNum = () => dig.reduce((acc, value, i) => acc + convert(value, num[i]), "")
-toSeg = () => num.reduce((acc, value, i) => acc + convert(value, dig[i]), "")
+toNum = () => invertedDigit.reduce((acc, pattern, i) => acc + convert(pattern, value[i]), "")
+toSeg = () => invertedValue.reduce((acc, pattern, i) => acc + convert(pattern, digit[i]), "")
 swapAB = () => swap(A0, B0, C) + swap(A1, B1, C) + swap(A2, B2, C) + swap(A3, B3, C)
 clearA = () => clear(DP) + clear(G) + clear(F) + clear(E) + clear(D) + clear(C) + clear(B) + clear(A)
 neg4 = (dst, carry, tmpA, tmpB, zero, one) =>
     toggle(dst[0]) + toggle(dst[1]) + toggle(dst[2]) + toggle(dst[3]) +
     set(one) + add4([one, zero, zero, zero], dst, carry, tmpA, tmpB)
-setNum = (i) => toNum() + swapAB() + clearA() + num[i].reduce((acc, bit) => acc + set(bit), "") + toSeg()
+setNum = (i) => toNum() + swapAB() + clearA() + value[i].reduce((acc, bit) => acc + set(bit), "") + toSeg()
 
 prog("0", setNum(0))
 prog("1", setNum(1))
